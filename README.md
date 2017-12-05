ece026f58d8c8a6fb291c83a3476cfd4d6c83df3
abdurrehim <abdurrehim.sen@bil.omu.edu.tr>
2017-12-05 20:07:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+followed below tutorial
+aşağıdaki tutoriali takip ettim
+
+https://medium.com/@avatsaev/angular-2-and-ruby-on-rails-user-authentication-fde230ddaed8
+
+Rails - Angular TypeScript login uygulamasıdır.
+
+devise_login_auth gem kullanıldı.
---
+++ frontend/angular-token-auth/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { HttpModule } from '@angular/http';
+
+import { MaterializeModule } from 'angular2-materialize';
+
+import { Angular2TokenService } from 'angular2-token';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    AuthDialogComponent,
+    LoginFormComponent,
+    RegisterFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    MaterializeModule
+  ],
+  providers: [Angular2TokenService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
